c9528980dc6cddb500935a98a2f3b57a22ecda6d
Zahira <zai.alvarez.l@gmail.com>
2022-03-29 11:12:12 +0200

    handleClickBtn

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ const winer = document.querySelector('.js-winer')
 const loser = document.querySelector('.js-loser')
 
 //Declarar funciones y eventos
-function handleClickBtn (){
-function cogerValor();
-function cogerValorRandom();
-function text();
+function handleClickBtn(event){
+ cogerValor();
+ cogerValorRandom();
+ text();
 }
 function text (){
 if (selectValue === random){
@@ -34,18 +34,16 @@ function cogerValorRandom(){
     return random;
 }
 
-
-    function getRandomNumber(max) { 
-    return Math.ceil(Math.random() * max); 
-      }
-
-
+function getRandomNumber(max) { 
+return Math.ceil(Math.random() * max); 
+}
 
 //Generar número aleatorio
 
   function generarNumeroAleatorio(max) {
     const aleatorio = parseInt (Math.random()*max);
     return aleatorio;
+}
 
 //Código que se ejecuta cuando carga la página
 
